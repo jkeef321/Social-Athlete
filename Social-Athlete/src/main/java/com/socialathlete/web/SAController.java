7f3d20508358b00aef11cd8a698e8d7233d389aa
j-keefer <jkeef321@hotmail.com>
2012-09-27 15:26:10 -0400

    got twitter to work... its a hack at this point

--- Social-Athlete/src/main/java/com/socialathlete/web/SAController.java
+++ Social-Athlete/src/main/java/com/socialathlete/web/SAController.java
@@ -2,22 +2,42 @@ package com.socialathlete.web;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.social.twitter.api.TimelineOperations;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @RequestMapping("/sa/**")
 @Controller
 public class SAController {
-
-    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+	
+	private final Twitter twitter = new TwitterTemplate();
+    
+	
+	@RequestMapping(method = RequestMethod.POST, value = "{id}")
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
     }
 
     @RequestMapping
-    public String index() {
-        return "sa/index";
+    public ModelAndView index() {
+        
+     	
+    	TimelineOperations timelineOps = twitter.timelineOperations();
+		List<Tweet> results = timelineOps.getUserTimeline("carlosvaldes5");
+    	
+		results.addAll(timelineOps.getUserTimeline("zacmacmath"));
+		
+    	return new ModelAndView("sa/index", "tweets", results);
     }
+    
+    
 }
