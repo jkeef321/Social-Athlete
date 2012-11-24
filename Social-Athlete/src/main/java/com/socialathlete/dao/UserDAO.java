4be08078c5e562d03924685a8cbd69e237da2db7
Justin Keefer <jkeef321@hotmail.com>
2012-11-24 12:44:54 -0500

    added additional DAO operations

--- Social-Athlete/src/main/java/com/socialathlete/dao/UserDAO.java
+++ Social-Athlete/src/main/java/com/socialathlete/dao/UserDAO.java
@@ -1,7 +1,12 @@
 package com.socialathlete.dao;
 
 import java.util.List;
+import com.socialathlete.domain.SAUser;
 
 public interface UserDAO {
 	public List<String> getFollowersByUserId(String userid);
+	
+	public List<String> getTeamsByLeague(String league);
+	
+	public SAUser getUserbyUsername(String username);
 }
--- Social-Athlete/src/main/java/com/socialathlete/dao/UserDAOImpl.java
+++ Social-Athlete/src/main/java/com/socialathlete/dao/UserDAOImpl.java
@@ -7,13 +7,19 @@ import javax.persistence.Query;
 import java.util.List;
 import java.util.Iterator;
 import java.util.ArrayList;
+import javax.persistence.NoResultException;
+
 
 import org.springframework.stereotype.Service;
+
+import com.socialathlete.domain.SAUser;
+import com.socialathlete.web.sausersignup.SAUserSignUpService;
 
 @Service
 public class UserDAOImpl implements UserDAO {
 	
 	private EntityManagerFactory emf;
+	
 	
 	@PersistenceUnit
 	public void setEntityManagerFactory(EntityManagerFactory emf) {
@@ -46,5 +52,60 @@ public class UserDAOImpl implements UserDAO {
 			
 		}
 	}
-
+	
+	public List<String> getTeamsByLeague(String league){
+		EntityManager em = this.emf.createEntityManager();
+		try {
+			Query query = em.createQuery("SELECT st.teamName  from SATeam st join st.league p where p.leagueName = ?1");
+			query.setParameter(1, league);
+			List results = query.getResultList();
+			ArrayList <String> result = new ArrayList();
+			if(results.size()!= 0)
+			{
+				Iterator it = results.iterator();
+				while(it.hasNext())
+				{
+					result.add((String) it.next());
+				}
+				
+			}
+			
+			return result;
+		}
+		finally {
+			if (em != null) {
+				em.close();
+			}
+			
+		}
+	}
+	
+	public SAUser getUserbyUsername(String username){
+		EntityManager em = this.emf.createEntityManager();
+		try {
+			
+			SAUser result = null;
+			
+			try{
+			Query query = em.createQuery("from SAUser u where u.username = ?1");
+			query.setParameter(1, username);
+			result = (SAUser) query.getSingleResult();
+		
+			}
+			catch(Exception e)
+			{
+				result = null;
+				return result;
+			}
+			
+			
+			return result;
+		}
+		finally {
+			if (em != null) {
+				em.close();
+			}
+			
+		}
+	}
 }
